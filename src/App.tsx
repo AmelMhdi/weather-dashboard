35a32de46d5d0816045ff2e2462b9fcdb9d62cbd
AmelMhdi <amel.m20@gmail.com>
2025-12-06 23:01:36 +0100

    responsive: desktop layout

--- src/App.tsx
+++ src/App.tsx
@@ -93,7 +93,7 @@ function App() {
         </div>
       </header>
 
-      <main>
+      <main className="max-w-3xl mx-auto">
         <section className="px-6 py-4 text-center font-bold text-3xl">How's the sky looking today?</section>
 
         <SearchBar onSearch={handleSearch} />
