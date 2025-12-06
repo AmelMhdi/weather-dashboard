b5ab1153e841b9108f827f4aa132ae2dd72b8ce2
AmelMhdi <amel.m20@gmail.com>
2025-12-06 14:14:31 +0100

    wip: main section style

--- src/App.tsx
+++ src/App.tsx
@@ -57,14 +57,21 @@ function App() {
   }, []);
 
   return (
-    <>
-      <div className='p-8 text-2xl bg-blue-500'>Weather Dashboard</div>
+    <div className="min-h-screen bg-gray-50 p-4">
+      <header className="px-6 py-4 text-xl font-bold text-gray-800 border-b border-gray-200">
+        Weather Now
+      </header>
 
-      <SearchBar onSearch={handleSearch} />
-      {loading && <p>Loading...</p>}
-      {error && <p>{error}</p>}
-      {weather && <CurrentWeather {...weather} />}
-    </>
+      <main>
+        <section className="px-6 py-4 text-center font-bold text-3xl">How's the sky looking today?</section>
+
+        <SearchBar onSearch={handleSearch} />
+        
+        {loading && <p>Loading...</p>}
+        {error && <p>{error}</p>}
+        {weather && <CurrentWeather {...weather} />}
+      </main>
+    </div>
   )
 }
 
